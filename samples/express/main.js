378e5869ab4ef533a3a727a2e48cb1602e2451ed
ledihildawan <lhildawan@gmail.com>
2026-07-05 21:49:46 +0700

    fix(samples): update express example for async/await + Express 5
    - Use env.render() + res.send() instead of res.render()
    - Express 5 requires explicit res.send() after render
    - Add proper async route handlers

--- samples/express/main.js
+++ samples/express/main.js
@@ -1,33 +1,28 @@
 import path from 'path';
-import nunjucks from '../..';
+import { fileURLToPath } from 'url';
+import nunjucks from '../../nunjucks/index.js';
 import express from 'express';
 
-const __dirname = import.meta.dirname;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const VIEWS = path.join(__dirname, 'views');
 
 const app = express();
-
-nunjucks.configure(path.join(__dirname, 'views'), {
-  autoescape: true,
-  express: app
-});
-
-app.use(express.static(__dirname));
-
-app.use((req, res, next) => {
-  res.locals.user = 'hello';
-  next();
+const env = new nunjucks.Environment(new nunjucks.FileSystemLoader(VIEWS), {
+  autoescape: true
 });
 
 app.get('/', async (req, res) => {
-  await res.render('index.html', {
+  const html = await env.render('index.html', {
     username: 'James Long <strong>copyright</strong>'
   });
+  res.send(html);
 });
 
 app.get('/about', async (req, res) => {
-  await res.render('about.html');
+  const html = await env.render('about.html');
+  res.send(html);
 });
 
 app.listen(4000, () => {
-  console.log('Express server running on http://localhost:4000');
+  console.log('Server running on http://localhost:4000');
 });
