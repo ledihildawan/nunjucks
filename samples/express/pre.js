49062b6b9bbcd4a668d440f7ffc729d832b4bc40
ledihildawan <lhildawan@gmail.com>
2026-07-05 13:44:35 +0700

    refactor: modernize samples/express for Bun + async-by-default
    - main.js: convert to ES6+ (const/let, arrow functions)
    - app.js: remove legacy comments, simplified config
    - extensions.js: convert RemoteExtension to ES6 class
    - pre.js: modern async precompilation script
    - views/index.html: update filter syntax to pipe (|>)
    
    Note: express rendering is now async-by-default

--- samples/express/pre.js
+++ samples/express/pre.js
@@ -2,27 +2,28 @@
 
 'use strict';
 
-var precompileString = require('../..').precompileString;
-var fs = require('fs');
-var path = require('path');
+const precompileString = require('../..').precompileString;
+const fs = require('fs');
+const path = require('path');
 
-
-var out = 'window.baseTmpl = ' +
-precompileString(
+const baseTmpl = precompileString(
   fs.readFileSync(path.join(__dirname, 'views/base.html'), 'utf-8'), {
     name: 'base.html',
     asFunction: true
   });
 
-out += 'window.aboutTmpl = ' +
-precompileString(
+const aboutTmpl = precompileString(
   fs.readFileSync(path.join(__dirname, 'views/about.html'), 'utf-8'), {
     name: 'about.html',
     asFunction: true
   });
 
+const out = 'window.baseTmpl = ' + baseTmpl + ';\nwindow.aboutTmpl = ' + aboutTmpl + ';';
+
 fs.writeFileSync(path.join(__dirname, 'js/templates.js'), out, 'utf-8');
 
-fs.writeFileSync(path.join(__dirname, 'js/nunjucks.js'),
+fs.writeFileSync(
+  path.join(__dirname, 'js/nunjucks.js'),
   fs.readFileSync(path.join(__dirname, '../../browser/nunjucks.js'), 'utf-8'),
-  'utf-8');
+  'utf-8'
+);
