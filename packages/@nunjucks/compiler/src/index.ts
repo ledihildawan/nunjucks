e4dba9cc31199efcad51e7768507e0aae19bf840
ledihildawan <lhildawan@gmail.com>
2026-07-21 18:31:00 +0700

    feat: @nunjucks/compiler with tree-shaking FP pattern
    Tree-shakable modular design:
    - emit.ts: String building utilities (Emitter class)
    - compile.ts: Main compilation (compile function)
    
    Tree-shaking imports:
    - import { compile } from '@nunjucks/compiler/compile'
    - import { createEmitter } from '@nunjucks/compiler/emit'

---
+++ packages/@nunjucks/compiler/src/index.ts
@@ -0,0 +1,5 @@
+// COMPILER - Nunjucks template compiler
+// Tree-shakable: import only what you need
+
+export { Emitter, createEmitter, emit, emitLine, getCode } from './emit.ts';
+export { compile } from './compile.ts';
