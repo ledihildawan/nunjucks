93955dfea806885fd3d3bb3547af9d0b40e8d2d3
ledihildawan <lhildawan@gmail.com>
2026-08-15 17:37:42 +0700

    chore(packages): remove orphan barrels with zero references (config, ansi, presentation)

--- packages/@nunjucks/core/src/config/index.ts
+++
@@ -1,9 +0,0 @@
-export type { DomPurifyConfig } from '../filter-bundle.ts';
-export type {
-  FilterBundle,
-  GlobalConfig,
-  SandboxEnvironment,
-  SandboxMode,
-  UndefinedMode,
-} from './global.ts';
-export { getDefaultConfig } from './global.ts';
